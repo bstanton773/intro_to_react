172a24b26351d93a9468a0af7e32fb4e15dff962
Brian <bstanton773@gmail.com>
2021-03-14 23:43:33 -0500

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Navbar from './components/Navbar';
+import {Switch, Route} from 'react-router-dom';
+import Home from './views/Home';
+import About from './views/About';
+import Contact from './views/Contact';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      name: 'Brian Stanton',
+      racers: []
+    }
+  }
+
+  handleSubmit = (e) =>{
+    e.preventDefault();
+    let year = e.target.year.value
+    let season = e.target.season.value
+    fetch(`https://ergast.com/api/f1/${year}/${season}/driverStandings.json`)
+    .then(res => res.json())
+    .then(data => {
+        this.setState({
+            racers: data.MRData.StandingsTable.StandingsLists[0].DriverStandings
+        }
+        )
+    })
+    .catch(error => console.log(error))
+
+    e.target.reset();
+  }
+
+  // componentDidMount(){
+  //   console.log("component mounted")
+  //   fetch('https://ergast.com/api/f1/2018/5/driverStandings.json')
+  //       .then(res => res.json())
+  //       .then(data => {
+  //           this.setState({
+  //               racers: data.MRData.StandingsTable.StandingsLists[0].DriverStandings
+  //           }
+  //           )
+  //       })
+  //       .catch(error => console.log(error))
+  // }
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <main className="container">
+          <Switch>
+            <Route exact path='/' render={() => <Home name={this.state.name} racers={this.state.racers} handleSubmit={this.handleSubmit}/>} />
+            <Route path='/about' render={() => <About name={this.state.name}/>} />
+            <Route path='/contact' render={() => <Contact name={this.state.name} />} />
+          </Switch>
+        </main>
+      </div>
+    )
+  }
 }
-
-export default App;
